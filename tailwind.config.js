b3f951d4f11508f2110910db7d459d61eec03f75
ferneynava <ferneynava@gmail.com>
2023-06-23 11:25:54 -0500

    :rocket: [Deploy] SpaceX-landing
    [Deploy] SpaceX-landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      sans: ['D-DIN', 'sans-serif']
+      sans: ['DINPro-Bold', 'sans-serif'],
+      serif: ['D-DIN', 'sans-serif']
     },
     extend: {}
   },
